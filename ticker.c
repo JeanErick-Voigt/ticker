c7d200f168cb524d8a584b0aed988b418ca0f37c
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-08-12 12:54:05 -0400

    Final commit, given extension by SSG Jaramillo

--- ticker.c
+++ ticker.c
@@ -21,55 +21,30 @@ int main(int argc, char ** argv)
 
 void file_parse(FILE *fp)
 {
+	char operand;
 	char symbol[6], name[64], price_change[10];
 	double price;
-//	int ret;
 	struct company *comp = NULL;
 	struct tree *root = NULL;
 	struct tree *current = NULL;
 	struct tree *root_by_price = NULL;
-//	struct tree *head = NULL;
 	while(fscanf(fp, "%s %lf", symbol, &price) != EOF && fgets(name, 64, fp)){
 
-		//fscanf(fp, "%s %lf %[^\n]", symbol, &dollar, name);
 
-		//malloc for company
 		comp  = stock_create(symbol, name, price);
-//		head = init_tree(comp);
 		
-//		printf("This is stock\n");
-//		printf("%s %lu %s\n", head->data->symbol, head->data->cents, head->data->name);
 		if(root == NULL){
 			root_by_price = current = root = init_tree(comp);
-			//printf("this is root if statement\n");
 		}
-//		printf("This is root\n");
-//		printf("%s %lu %s\n", root->data->symbol, root->data->cents, root->data->name);
-		//malloc for tree
-		//insert into tree
-		//struct tree *head = NULL;
-		//struct tree *t = NULL
 		tree_insert(root, comp); 
-		//printf("This is tree insert test\n");
-		//printf("%s %lu %s\n", head->data->symbol, head->data->cents, head->data->name);
-		//printf("This is test statement\n");
-		//printf("RET = %d\n", ret);
-		//printf(" SYMBOL: %s DOLLAR: %lf  NAME: %s\n", symbol, price, name); 
 
 	}
 	inorder(root);
-//	while(fgets(buffer, 100, stdin)){
-//		sscanf(buffer, "%s %s", symbol, price_change);
-//	}
-//	printf("last symbol and price_change  %s %s\n", symbol, price_change);
+	operand = search_and_update(root, price_change, symbol);
 	
-	search_and_update(root, price_change, symbol);
-	
-//	added lines of code
 	tree_insert_by_price(root_by_price, current, root);
 	printf("\n");
 	inorder(root_by_price);
-	//fclose(fp);
 }
 
 
@@ -90,7 +65,7 @@ int conversion(char price[])
 	num = atoi(buffer);
 	return(num);
 }
-void search_and_update(struct tree *root, char price_change[10], char symbol[6])
+char search_and_update(struct tree *root, char price_change[10], char symbol[6])
 {
 	char operand;
 	int price;
@@ -100,33 +75,13 @@ void search_and_update(struct tree *root, char price_change[10], char symbol[6])
 		if(price_change[0] == '+' || price_change[0] == '-'){
 			operand = price_change[0];
 			printf("Operand %c\n", operand);
-			//price = conversion(price_change[10]);
-			//printf("%d\n", price);
 		}
 		price = conversion(price_change);
 		price *= 100;
 		printf("price %d \n", price);
-		tree_search(root, symbol, price);
+		tree_search(root, symbol, price, operand);
 	}
-	//price = conversion(price_change);
-	//printf("price %d", price);
-	//printf("%s %s\n", symbol, price_change);
-	//int length = strlen(price_change);
-	//buffer[0] = '\0';
-	//int buffer_i = 0;
-	//for(int i = 0; i <= length; i++){
-	//	if(price_change[i] >= '0' &&  price_change[i] <= '9'){
-	//		buffer[buffer_i++] = price_change[i];
-	//		printf("buffer index value %c\n", buffer[i]);
-	//	}
-	//}
-	//buffer[buffer_i] = '\0';
-	//int num;
-	//num = atoi(buffer);
-	//printf("This is number buffer %d\n", num);
-	//if(price_change[0] == '+')
-	//	printf("%d\n", operand);
-	//printf("String length of price_change is %d\n", strlen(price_change));
+	return operand;
 }
 struct tree *copy_init_tree(struct tree *current)
 {
@@ -168,20 +123,20 @@ struct company *stock_create(char *symbol, char *name, double price)
 }
 
 
-void tree_search(struct tree *root, char word[], int price)
+void tree_search(struct tree *root, char word[], int price, char operand)
 {
 	int value;
 	value = strcasecmp(word, root->data->symbol);
 	if(value < 0){
 		if(root->left != NULL){
-			tree_search(root->left, word, price);
+			tree_search(root->left, word, price, operand);
 		}else{
 			printf("word does not exist\n");
 			return;
 		}
 	}else if(value > 0){
 		if(root->right != NULL){
-			tree_search(root->right, word, price);
+			tree_search(root->right, word, price, operand);
 		}else{
 			printf("word does not exist\n");
 			return;
@@ -189,7 +144,11 @@ void tree_search(struct tree *root, char word[], int price)
 	}else{
 		printf("before update %lu\n", root->data->cents);
 		printf("This is price %d\n", price);
-		root->data->cents = root->data->cents + price;
+		if(operand == '+'){
+			root->data->cents = root->data->cents + price;
+		}else{
+			root->data->cents = root->data->cents - price;
+		}
 		printf("updated cents %lu", root->data->cents);
 		
 	}
@@ -207,7 +166,7 @@ void tranverse_tree(struct tree *root, struct tree *root_by_price, struct tree *
 
 void tree_insert_by_price(struct tree * root_by_price, struct tree *current, struct tree *root)
 {
-		
+
 	if(root_by_price->data->cents < current->data->cents){
 		if (root_by_price->left != NULL){
 			tree_insert_by_price(root_by_price->left, current, root);
@@ -225,7 +184,6 @@ void tree_insert_by_price(struct tree * root_by_price, struct tree *current, str
 		;
 	}
 }
-//*/
 void tree_insert(struct tree *root, struct company *compan)
 {
 	int value;
@@ -235,15 +193,12 @@ void tree_insert(struct tree *root, struct company *compan)
 			tree_insert(root->left, compan);
 		}else{
 			root->left = init_tree(compan);
-//			printf("Print root %s and root->left %s\n", root->data->symbol, root->left->data->symbol);
 		}
 	}else if (value > 0){
 		if(root->right != NULL) {
 			tree_insert(root->right, compan);
 		}else{
 			root->right = init_tree(compan);
-//			printf("Print root %s and root->right %s\n", root->data->symbol, root->right->data->symbol);
-			//root->right;
 		}
 	}else{
 		return;
@@ -253,16 +208,12 @@ void tree_insert(struct tree *root, struct company *compan)
 
 void inorder(struct tree *root)
 {
-	//printf("entering the print statement\n");
 	if(root != NULL)
 	{
 		inorder(root->left);
 		printf("%s %lu %s", root->data->symbol, root->data->cents, root->data->name);
-		//inorder(root->left);
 		inorder(root->right);
 	}
-	//printf("\n")
-	//printf("%s %lu %s \n", root->data->symbol, root->data->cents, root->data->name);
 }
 
 
